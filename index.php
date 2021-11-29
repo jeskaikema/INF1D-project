0a7425ac9234675d270c8e0e352beb4f4bdbe41c
VictorP03 <crapowner321@gmail.com>
2021-11-29 14:53:45 +0100

    gitignore aangepast

--- index.php
+++ index.php
@@ -1,27 +1,10 @@
-<!doctype html>
-<html class="no-js" lang="">
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <title></title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/main.css">
-
-  <meta name="theme-color" content="#fafafa">
+	<meta charset="UTF-8">
+	<title>TicketGuru</title>
 </head>
-
 <body>
-
-  <!-- Add your site or application content here -->
-  <p>Hello world! This is HTML5 Boilerplate.</p>
-  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
-  <script src="js/plugins.js"></script>
-  <script src="js/main.js"></script>
-
+	
 </body>
-
 </html>
