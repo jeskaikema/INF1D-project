da4673d467efb8ae3bc4f46d81533ec17647f7a0
VictorP03 <crapowner321@gmail.com>
2021-12-01 14:56:29 +0100

    primitief login systeem gebouwd, session file gemaakt, database link gemaakt en een check gemaakt of iemand wel of niet ingelogd is

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 	<form action="src/login.php" method="POST">
 		<label for="email">Email:</label><br>
 		<input type="email" name="email" id="email" placeholder="emailadres"><br>
-		<label for="password">Wachtwoord:</label><br>
-		<input type="password" name="password" id="password" placeholder="wachtwoord"><br>
 		<input type="submit" name="submit" value="inloggen">
 	</form>	
 </body>
--- src/login.php
+++ src/login.php
@@ -1,11 +1,32 @@
 <?php
-    if(isset($_POST['submit'])) 
+    include_once "../config/config.php";
+    include_once "../helper/loggedin.php";
+    $email = isset($_POST['email']) ? $_POST['email'] : NULL;
+    if (empty($email)) 
     {
-        $email = isset($_POST['email']) ? $_POST['email'] : NULL;
-        $password = isset($_POST['password']) ? $_POST['password'] : NULL;
-        if (empty($email) || empty($password)) 
+        echo "vul alle vakken in";
+        return 1;
+    }
+    $name = explode('@', $email);
+    $fName = explode('.', $name[0]);
+    $query = "SELECT `email` FROM users WHERE `email` = ?";
+    if ($statement = mysqli_prepare($conn, $query)) {
+        mysqli_stmt_bind_param($statement, 's', $email);
+        if (!mysqli_stmt_execute($statement))
         {
-            echo "vul alle vakken in";
-            return 1;
+            DIE("EXECUTE ERROR");
         }
+        mysqli_stmt_store_result($statement);
+        if (mysqli_stmt_num_rows($statement) == 0) 
+        {
+            DIE("USER DOES NOT EXIST");
+        }
+        include "../helper/session.php";
+        $_SESSION['LoggedIn'] = true;
+        $_SESSION['name'] = ucfirst($fName[0]);
+        echo "Welkom, " . $_SESSION['name'] . ".";
+    }
+    else 
+    {
+        DIE("PREPARE ERROR");
     }
