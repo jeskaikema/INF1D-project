8b8deb7b9c7d319a2c6668474648db8dfbd3edec
VictorP03 <crapowner321@gmail.com>
2021-12-01 15:09:29 +0100

    Redirect bug gefixed met login systeem

--- src/login.php
+++ src/login.php
@@ -1,33 +1,40 @@
 <?php
     include_once "../config/config.php";
-    include_once "../helper/loggedin.php";
-    $email = isset($_POST['email']) ? $_POST['email'] : NULL;
-    if (empty($email)) 
+    if (isset($_POST['submit'])) 
     {
-        echo "vul alle vakken in";
-        return 1;
-    }
-    $name = explode('@', $email);
-    $fName = explode('.', $name[0]);
-    $query = "SELECT `email` FROM users WHERE `email` = ?";
-    if ($statement = mysqli_prepare($conn, $query)) {
-        mysqli_stmt_bind_param($statement, 's', $email);
-        if (!mysqli_stmt_execute($statement))
+        $email = isset($_POST['email']) ? $_POST['email'] : NULL;
+        if (empty($email)) 
         {
-            DIE("EXECUTE ERROR");
+            echo "vul alle vakken in";
+            return 1;
         }
-        mysqli_stmt_store_result($statement);
-        if (mysqli_stmt_num_rows($statement) == 0) 
+        $name = explode('@', $email);
+        $fName = explode('.', $name[0]);
+        $query = "SELECT `email` FROM users WHERE `email` = ?";
+        if ($statement = mysqli_prepare($conn, $query)) 
         {
-            DIE("USER DOES NOT EXIST");
+            mysqli_stmt_bind_param($statement, 's', $email);
+            if (!mysqli_stmt_execute($statement))
+            {
+                DIE("EXECUTE ERROR");
+            }
+            mysqli_stmt_store_result($statement);
+            if (mysqli_stmt_num_rows($statement) == 0) 
+            {
+                DIE("USER DOES NOT EXIST");
+            }
+            include "../helper/session.php";
+            $_SESSION['LoggedIn'] = true;
+            $_SESSION['name'] = ucfirst($fName[0]);
+            $_SESSION['email'] = $email;
+            echo "Welkom, " . $_SESSION['name'] . ".";
         }
-        include "../helper/session.php";
-        $_SESSION['LoggedIn'] = true;
-        $_SESSION['name'] = ucfirst($fName[0]);
-        $_SESSION['email'] = $email;
-        echo "Welkom, " . $_SESSION['name'] . ".";
+        else
+        {
+            DIE("PREPARE ERROR");
+        }
     }
     else 
     {
-        DIE("PREPARE ERROR");
+        header("location: ../index.php");
     }
