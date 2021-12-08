94899f4d938e7f04a4b12b11e7d69c842553b18e
JonathanHeezen <jonathan.heezen@student.nhlstenden.com>
2021-12-08 10:48:32 +0100

    uppdate navbar

---
+++ Sidebar.php
@@ -0,0 +1,10 @@
+<?php
+include "index.php";
+?>
+    <div class= Sidebar>
+        <img src="logo-nhl-stenden-2018 wit2" alt= witLogo>
+        <div class= Background>
+
+        </div>
+
+    </div>
