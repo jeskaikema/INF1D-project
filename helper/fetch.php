18897fd36c937a324687e53a440dac199e6fa804
VictorP03 <crapowner321@gmail.com>
2021-12-08 15:12:59 +0100

    functies begonnen

---
+++ helper/fetch.php
@@ -0,0 +1,21 @@
+<?php
+
+    function getAllTickets() 
+    {
+        $query = "SELECT * FROM 'ticket' LEFT JOIN `order` ON ticket.id = ticket_id LEFT JOIN `Room` ON ticket.room_number = room.room_number";
+    }
+
+    function getAllOrders() 
+    {
+        $query = "SELECT `User_Email`, Room FROM `ticket`";
+    }
+
+    function getAllReservations() 
+    {
+
+    }
+
+    function getAllIssues() 
+    {
+
+    }
