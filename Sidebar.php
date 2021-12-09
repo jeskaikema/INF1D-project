d846e0ff490e89cc721c7a6cb4b38832474a8628
JonathanHeezen <jonathan.heezen@student.nhlstenden.com>
2021-12-09 15:51:16 +0100

    Sidebar Uppdate
    achtergrond compleet en logo op de goede plek met wat nieuwe divs voor elke pagina.

--- Sidebar.php
+++ Sidebar.php
@@ -1,10 +1,53 @@
-<?php
-include "index.php";
-?>
-    <div class= Sidebar>
-        <img src="logo-nhl-stenden-2018 wit2" alt= witLogo>
-        <div class= Background>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>TicketGuru</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class=page>
+    <div class="Header">
+    
+    </div>
 
+    <div class="Sidebar"> 
+        <div class="S-Background">     
+            <img src="img/logo-nhl-stenden-2018 wit2.png" alt= witLogo width="102.6px" height="102.6px" style="margin-left:25px;margin-top:auto">
+            <div class="S.1">
+                <div class="Dashboard">
+                    <h4>Dashboard</h4>
+                    <img src="img/dashboard.png" alt=Dashboard>
+                </div>
+                <div class="S-Mijn tickets">
+                    <h4>Mijn Tickets</h4>
+                    <img src=" " alt=Dashboard>
+                </div>
+                <div class="S-Maak melding">
+                    <h4>Maak melding</h4>
+                    <img src="" alt=Dashboard>
+                </div>
+                <div class="S-Reserveren">
+                    <h4>Reserveren</h4>
+                    <img src="" alt=Dashboard>
+
+                </div>
+                <div class="S-Data privacy">
+                    <h4>Data privacy</h4>
+
+                </div>
+                <div class="S-Bestellingen">
+
+                </div>
+            </div>
+            <div class="S-2">
+            </div>
         </div>
+    </div>
+    
+    <div class="main">
 
     </div>
+
+    </body>
+</html>
