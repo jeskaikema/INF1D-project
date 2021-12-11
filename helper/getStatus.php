586bb8f779e6cf040020352c99997366e42938e5
VictorP03 <crapowner321@gmail.com>
2021-12-11 20:00:46 +0100

    getStatus.php changes toegevoegd

--- helper/getStatus.php
+++ helper/getStatus.php
@@ -1,7 +1,6 @@
 <?php
 function getStatus($conn, $ID)
 {
-    //include "../config/config.php";
     $query = "SELECT `Status` FROM `ticket` WHERE `ID` = ?";
     if ($statement = mysqli_prepare($conn, $query)) {
         mysqli_stmt_bind_param($statement, 's', $ID);
