a0bda597ffa64e0d6084211bccbf35801e38395e
JonathanHeezen <jonathan.heezen@student.nhlstenden.com>
2021-12-13 13:54:15 +0100

    Aanpassisngen nav-bar
    commit van vrijdag (vergeten)

--- Sidebar.php
+++ Sidebar.php
@@ -11,39 +11,36 @@
     
     </div>
 
-    <div class="Sidebar"> 
-        <div class="S-Background">     
+    <div class="Sidebar">      
             <img src="img/logo-nhl-stenden-2018 wit2.png" alt= witLogo width="102.6px" height="102.6px" style="margin-left:25px;margin-top:auto">
-            <div class="S.1">
-                <div class="Dashboard">
+            <div class="S-1">
+                <div class="S-Item">
+                    <img src="img/dashboard.png" alt=Dashboard width="20px" height="21px">
                     <h4>Dashboard</h4>
-                    <img src="img/dashboard.png" alt=Dashboard>
                 </div>
-                <div class="S-Mijn tickets">
+                <div class="S-Item">
+                    <img src="img/mijn tickets.png" alt=Mijn tickets width="19px" height="18px"> 
                     <h4>Mijn Tickets</h4>
-                    <img src=" " alt=Dashboard>
                 </div>
-                <div class="S-Maak melding">
+                <div class="S-Item">
+                    <img src="img/maak melding.png" alt=Maak melding width="19px" height="18px">
                     <h4>Maak melding</h4>
-                    <img src="" alt=Dashboard>
                 </div>
-                <div class="S-Reserveren">
+                <div class="S-Item">
+                    <img src="img/reserveren.png" alt=Reserveren width="19px" height="18px">
                     <h4>Reserveren</h4>
-                    <img src="" alt=Dashboard>
-
                 </div>
-                <div class="S-Data privacy">
+                <div class="S-Item">
+                    <img src="img/privacy.png" alt=Privacy width="12px" height="18px">
                     <h4>Data privacy</h4>
+                </div>
+                <div class="S-Item">
 
                 </div>
-                <div class="S-Bestellingen">
-
-                </div>
-            </div>
             <div class="S-2">
             </div>
-        </div>
     </div>
+</div>
     
     <div class="main">
 
