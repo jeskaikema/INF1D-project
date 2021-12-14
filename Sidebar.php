f56326f691b5e028cf10a5c6215d906bcf953719
VictorP03 <crapowner321@gmail.com>
2021-12-14 18:49:42 +0100

    frontend aangepast zodat alles beter past, en links toegevoegd

--- Sidebar.php
+++ Sidebar.php
@@ -16,47 +16,45 @@
             <div class="S-1">
                 <div class="S-Item">
                     <img src="img/dashboard.png" alt=Dashboard width="20px" height="21px" style="margin:auto; margin-left: 8px; margin-right: 4px;">
-                    <h4>Dashboard</h4>
+                    <a href="#"><h4>Dashboard</h4></a>
                 </div>
                 <div class="S-Item">
                     <img src="img/mijn tickets.png" alt=Mijn tickets width="19px" height="18px" style="margin:auto; margin-left: 8px; margin-right: 4px;"> 
-                    <h4>Mijn Tickets</h4>
+                    <a href="#"><h4>Mijn Tickets</h4></a>
                 </div>
                 <div class="S-Item">
                     <img src="img/maak melding.png" alt=Maak melding width="19px" height="18px" style="margin:auto; margin-left: 8px; margin-right: 4px;">
-                    <h4>Maak melding</h4>
+                    <a href="#"><h4>Maak melding</h4></a>
                 </div>
                 <div class="S-Item">
                     <img src="img/reserveren.png" alt=Reserveren width="19px" height="18px" style="margin:auto; margin-left: 8px; margin-right: 4px;">
-                    <h4>Reserveren</h4>
+                    <a href="#"><h4>Reserveren</h4></a>
                 </div>
                 <div class="S-Item">
                     <img src="img/privacy.png" alt=Privacy width="12px" height="18px" style="margin:auto; margin-left: 12px; margin-right: 8px;">
-                    <h4>Data privacy</h4>
+                    <a href="#"><h4>Data privacy</h4></a>
                 </div>
                 <div class="S-Item">
                     <img src="img/bestelling.png" alt=bestelling width="19px" height="18px" style="margin:auto; margin-left: 8px; margin-right: 5px;">
-                    <h4>Bestellingen</h4>   
+                    <a href="#"><h4>Bestellingen</h4></a>   
                 </div>
             </div>
             
 
             <div class="S-2">
-
                 <div class="S-Item">
-                    <h4>Over ons</h4>
+                    <a href="#"><h4>Over ons</h4></a>
                 </div>
                 <div class="S-Item">
-                    <h4>Need help?</h4>
+                    <a href="#"><h4>Need help?</h4></a>
                 </div>
             </div>
 
             <div class="S-3">
                 <div class="S-Item">
-                    <h5>Uitloggen</h5>
+                    <a href="#"><h5>Uitloggen</h5></a>
                 </div>   
-            </div>
-            
+            </div>   
         </div>
         
         <div class="main">
