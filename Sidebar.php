bf39f234411ce2413aea358632580877a066149b
JonathanHeezen <jonathan.heezen@student.nhlstenden.com>
2021-12-15 16:37:02 +0100

    sidebar final

--- Sidebar.php
+++ Sidebar.php
@@ -12,30 +12,30 @@
         </div>
 
         <div class="Sidebar">      
-            <img src="img/logo-nhl-stenden-2018 wit2.png" alt= witLogo width="102.6px" height="102.6px" style="margin-left:25px;margin-top:auto">
+            <img src="img/logo-nhl-stenden-2018 wit2.png" alt= witLogo width="133px" height="128px" style="margin-left:25px;margin-top:15px">
             <div class="S-1">
                 <div class="S-Item">
-                    <img src="img/dashboard.png" alt=Dashboard width="20px" height="21px" style="margin:auto; margin-left: 8px; margin-right: 4px;">
+                    <img src="img/dashboard.png" alt=Dashboard width="24.8px" height="25.6px" style="margin:auto; margin-left: 8px; margin-right: 20px;">
                     <a href="#"><h4>Dashboard</h4></a>
                 </div>
                 <div class="S-Item">
-                    <img src="img/mijn tickets.png" alt=Mijn tickets width="19px" height="18px" style="margin:auto; margin-left: 8px; margin-right: 4px;"> 
+                    <img src="img/mijn tickets.png" alt=Mijn tickets width="26px" height="28px" style="margin:auto; margin-left: 8px; margin-right: 18px;"> 
                     <a href="#"><h4>Mijn Tickets</h4></a>
                 </div>
                 <div class="S-Item">
-                    <img src="img/maak melding.png" alt=Maak melding width="19px" height="18px" style="margin:auto; margin-left: 8px; margin-right: 4px;">
+                    <img src="img/maak melding.png" alt=Maak melding width="26px" height="28px" style="margin:auto; margin-left: 8px; margin-right: 18px;">
                     <a href="#"><h4>Maak melding</h4></a>
                 </div>
                 <div class="S-Item">
-                    <img src="img/reserveren.png" alt=Reserveren width="19px" height="18px" style="margin:auto; margin-left: 8px; margin-right: 4px;">
+                    <img src="img/reserveren.png" alt=Reserveren width="32px" height="29px" style="margin:auto; margin-left: 8px; margin-right: 14px;">
                     <a href="#"><h4>Reserveren</h4></a>
                 </div>
                 <div class="S-Item">
-                    <img src="img/privacy.png" alt=Privacy width="12px" height="18px" style="margin:auto; margin-left: 12px; margin-right: 8px;">
+                    <img src="img/privacy.png" alt=Privacy width="15px" height="26px" style="margin:auto; margin-left: 12px; margin-right: 26px;">
                     <a href="#"><h4>Data privacy</h4></a>
                 </div>
                 <div class="S-Item">
-                    <img src="img/bestelling.png" alt=bestelling width="19px" height="18px" style="margin:auto; margin-left: 8px; margin-right: 5px;">
+                    <img src="img/bestelling.png" alt=bestelling width="25px" height="27px" style="margin:auto; margin-left: 8px; margin-right: 22px;">
                     <a href="#"><h4>Bestellingen</h4></a>   
                 </div>
             </div>
