fb6b7ed10746bd6e2369c9b0c52dbdc8851f2812
VictorP03 <crapowner321@gmail.com>
2021-12-21 13:42:23 +0100

    functie voor veranderen ticketstatus gemaakt

--- src/close_ticket.php
+++ src/close_ticket.php
@@ -1,24 +1,16 @@
 <?php
     function closeTicket($conn, $id) 
     {
-    $query = "UPPDATE `Status` FROM `ticket` WHERE `ID` = ?";
-        if isset($_POST(["submit"])) {     
-        $closeticket = true;
-        }   
-                
-        $status = gesloten;
+        $query = "UPDATE `ticket` 
+                  SET `Status` = 'closed' 
+                  WHERE `ID` = ?";   
+        // $closeticket = true;   
 
         if ($statement = mysqli_prepare($conn, $query)) {
-            mysqli_stmt_bind_param($statement, 's', $ID);
-            if (mysqli_stmt_execute($statement)) {
-                mysqli_stmt_bind_result($statement, $status);
-                if (mysqli_stmt_fetch($statement)) {
-                    return $status;
-                }
-            } else {
-                die("EXECUTE ERROR");
-            }
-        } else {
-            die(mysqli_error($conn));
+            mysqli_stmt_bind_param($statement, 'i', $ID);
+        }
+
+        if (!mysqli_stmt_execute($statement)) {
+            DIE("EXECUTE ERROR");
         }
     }
