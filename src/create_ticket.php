1a087917f88b0b4ff3f61d3bcd475a94bb8eb0c1
VictorP03 <crapowner321@gmail.com>
2022-01-12 14:55:12 +0100

    kan execute error niet oplossen

--- src/create_ticket.php
+++ src/create_ticket.php
@@ -3,30 +3,38 @@ include_once "../config/config.php";
 include_once "../helper/validateFile.php";
 include_once "../helper/checkPhoneNumber.php";
 
-function placeTicket($conn, $phonenumber, $email, $roomnumber, $file)
+function placeTicket($conn, $phonenumber, $email, $roomnumber, $description, $file)
 {
-    $query = "INSERT INTO 'ticket' (Phone_Number, User_Email, Room_ID, Description, File) VALUES (?, ?, ?, ?, ?)";
+    $query = "INSERT INTO `ticket` (User_Email, Room_ID, Phone_Number, `Description`, `File`) VALUES (?, ?, ?, ?, ?)";
 
-    if ($stmt = mysqli_prepare($conn, $query)) {
-        mysqli_stmt_bind_param($stmt, 'isiss', $phonenumber, $email, $roomnumber, $description, $file);
+    if ($stmt = mysqli_prepare($conn, $query)) 
+    {
+        mysqli_stmt_bind_param($stmt, 'siiss', $email, $roomnumber, $phonenumber, $description, $file);
+    }
 
-        if (!mysqli_stmt_execute($stmt)) {
-            die ("Execute error!");
-        }
+    if (!mysqli_stmt_execute($stmt)) 
+    {
+        DIE("Execute error!");
     }
 }
 
 if (isset($_POST['submit'])) {
     $phonenumber = $_POST['phonenumber'];
+    $description = $_POST['report'];
     $email = (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) ? $_POST['email'] : "invalid";
     $roomnumber = $_POST['roomnumber'];
     $file = $_FILES['file'];
 }
 
-if (!checkPhoneNumber($phonenumber)) {
-    header("location: ../pages/orderForm.php?error=invalidPhoneNumber");
-    exit();
-}
+// if (empty($description) || empty($location) || empty($price))
+// {
+//     header("location: ../pages/orderForm.php?error=emptyField");
+// }
+
+// if (!checkPhoneNumber($phonenumber)) {
+//     header("location: ../pages/orderForm.php?error=invalidPhoneNumber");
+//     exit();
+// }
 
 if (file_exists($file['tmp_name']) || is_uploaded_file($file['tmp_name'])) {
     if (validateFile($file, "create_ticket.php") == 1) {
@@ -37,4 +45,4 @@ if (file_exists($file['tmp_name']) || is_uploaded_file($file['tmp_name'])) {
     $file['name'] = NULL;
 }
 
-placeTicket($conn, $phonenumber, $email, $roomnumber, $file['name']);
+placeTicket($conn, $phonenumber, $email, $roomnumber, $description, $file['name']);
