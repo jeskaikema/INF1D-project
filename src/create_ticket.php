7b42c0fe1a9b240f6eff5573ccef80a036420824
Thom-Neijmeijer <Thom Neijmeijer>
2022-01-12 16:31:09 +0100

    adjusted according to database requirements

--- src/create_ticket.php
+++ src/create_ticket.php
@@ -3,18 +3,17 @@ include_once "../config/config.php";
 include_once "../helper/validateFile.php";
 include_once "../helper/checkPhoneNumber.php";
 
-function placeTicket($conn, $phonenumber, $email, $roomnumber, $description, $file)
+function placeTicket($conn, $phonenumber, $email, $roomnumber, $description, $file, $priority, $location)
 {
-    $query = "INSERT INTO `ticket` (User_Email, Room_ID, Phone_Number, `Description`, `File`) VALUES (?, ?, ?, ?, ?)";
+    $query = "INSERT INTO `ticket` (User_Email, Room_ID, Phone_Number, `Description`, `File`, `Status`, `Priority`, `Location`, Ticket_Date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-    if ($stmt = mysqli_prepare($conn, $query)) 
-    {
-        mysqli_stmt_bind_param($stmt, 'siiss', $email, $roomnumber, $phonenumber, $description, $file);
+    if ($stmt = mysqli_prepare($conn, $query)) {
+
+        mysqli_stmt_bind_param($stmt, 'siisssiss', $email, $roomnumber, $phonenumber, $description, $file, $status, $priority, $location, $date);
     }
 
-    if (!mysqli_stmt_execute($stmt)) 
-    {
-        DIE("Execute error!");
+    if (!mysqli_stmt_execute($stmt)) {
+        die("Execute error!");
     }
 }
 
@@ -24,17 +23,20 @@ if (isset($_POST['submit'])) {
     $email = (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) ? $_POST['email'] : "invalid";
     $roomnumber = $_POST['roomnumber'];
     $file = $_FILES['file'];
+    $priority = 3;
+    $location = $_POST['branch'];
+    $status = "nieuw";
+    $date = date('Y-m-d H:i:s \G\M\T', time());
 }
 
-// if (empty($description) || empty($location) || empty($price))
-// {
-//     header("location: ../pages/orderForm.php?error=emptyField");
-// }
-
-// if (!checkPhoneNumber($phonenumber)) {
-//     header("location: ../pages/orderForm.php?error=invalidPhoneNumber");
-//     exit();
-// }
+//if (empty($description) || empty($location) || empty($price)) {
+//    header("location: ../pages/orderForm.php?error=emptyField");
+//}
+//
+//if (!checkPhoneNumber($phonenumber)) {
+//    header("location: ../pages/orderForm.php?error=invalidPhoneNumber");
+//    exit();
+//}
 
 if (file_exists($file['tmp_name']) || is_uploaded_file($file['tmp_name'])) {
     if (validateFile($file, "create_ticket.php") == 1) {
@@ -45,4 +47,4 @@ if (file_exists($file['tmp_name']) || is_uploaded_file($file['tmp_name'])) {
     $file['name'] = NULL;
 }
 
-placeTicket($conn, $phonenumber, $email, $roomnumber, $description, $file['name']);
+placeTicket($conn, $phonenumber, $email, $roomnumber, $description, $file['name'], $status, $priority, $location, $date);
